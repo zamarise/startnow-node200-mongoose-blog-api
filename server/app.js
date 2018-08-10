484d2400e0d6dc6594ce0a7cf77ba5751bc1df59
Zamarise Montoya <zamarise@gmail.com>
2018-08-09 20:15:30 -0700

    Add Continuous Integration

--- server/app.js
+++ server/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
+// console.log(process.env.MY);
 mongoose.connect(
-  'mongodb://127.0.0.1:27017/my-blog',
+  process.env.ENV === 'production' ? process.env.MONGO_URI : 'mongodb://127.0.0.1:27017/my-blog',
   { useNewUrlParser: true }
 );
 mongoose.Promise = Promise;
